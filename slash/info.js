2a94e7dee922bf3f191ac52e5eab772f356a7466
Brian <le.brian125@gmail.com>
2022-05-10 17:56:26 -0700

    initial push

--- slash/info.js
+++ slash/info.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
 
+
 module.exports = {
 	data: new SlashCommandBuilder().setName("info").setDescription("Displays info about the currently playing song"),
 	run: async ({ client, interaction }) => {
